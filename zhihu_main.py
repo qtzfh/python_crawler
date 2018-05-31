5cafb10c6b6e9a4080114dcd36df6262a8acb27e
qtzfh <qtzfh@hotmail.com>
2018-06-01 00:11:42 +0800

    修改错误的方法

--- zhihu_main.py
+++ zhihu_main.py
@@ -113,7 +113,7 @@ def test():
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
-    scheduler.add_job(test, 'cron', day_of_week='0-6', hour=00, minute=10, second=00)
+    scheduler.add_job(task_all_work, 'cron', day_of_week='0-6', hour=00, minute=10, second=00)
     scheduler2 = BackgroundScheduler()
     scheduler2.add_job(tick, 'interval', seconds=30)
     try:
